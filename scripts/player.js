012c2996afcf60401a170f12d5587fec1b32a168
MEPC\Divit <dsharma5095@gmail.com>
2017-08-10 22:41:47 -0400

    Added popup exit button

--- scripts/player.js
+++ scripts/player.js
@@ -1,8 +1,13 @@
 
 var popup = document.getElementById('VideoPopup');
-popup.style.visibility = 'hidden';
+popup.classList.add("visible");
+
+var exit_popup = popup.querySelector('.popup-exit-btn');
+exit_popup.addEventListener('click', function (ev) {
+  popup.classList.remove('visible')
+});
 
 var play_trkr = document.querySelector('.obj-trkr-graphic button');
 play_trkr.addEventListener('click', function (ev) {
-  popup.style.visibility = 'visible';
-})
+  popup.classList.add('visible');
+});
